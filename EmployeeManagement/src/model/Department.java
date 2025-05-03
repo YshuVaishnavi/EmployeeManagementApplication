24a4df01d48b16338994bc34e8897950192c1561
Yshú <139226918+YshuVaishnavi@users.noreply.github.com>
2025-05-03 12:49:11 +0530

    Add files via upload

---
+++ EmployeeManagement/src/model/Department.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Department { // dept is a table
+
+	 private int deptid;              //entity
+	 private String deptname;
+	 //encapsulation
+	public int getDeptid() {
+		return deptid;
+	}
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	 
+	 
+}
